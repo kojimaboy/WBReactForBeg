925b07caefb50c41606a28040ee4dc4e3d691499
kojimaboy <kojimaboy@gmail.com>
2021-04-27 23:25:01 -0700

    updated packages and changed to function components

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,18 +3,16 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 
-class App extends React.Component{
-    render(){
-        return(
-            <div className="catch-of-the-day">
-                <div className="menu">
-                    <Header tagline="Fresh Seafood Market" age={100} />
-                </div>
-                <Order />
-                <Inventory />
-            </div>
-        )   
-    }
+const App = () => { 
+  return(
+    <div className="catch-of-the-day">
+       <div className="menu">
+          <Header tagline="Fresh Seafood Market" age={100} />
+       </div>
+          <Order />
+          <Inventory />
+       </div>
+    )   
 }
 
 export default App;
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-class Order extends React.Component {
-  render() {
-    return <div className="order">Order!!!</div>;
-  }
+const Order = () => {
+    return(
+      <div className="order">Order!!!</div>
+    );
 }
 
 export default Order;
